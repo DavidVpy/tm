f7cb396c467cfe92ee7cbe71efc101ed3fcbce99
DavidVpy <fdvpfdvp@gmail.com>
2026-03-01 02:31:18 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 // ============================================
 // CONFIGURACIÓN - API URL
 // ============================================
-// IMPORTANTE: Después de implementar en Apps Script,
-// reemplazá esta URL con la que te dé Apps Script
-
 const API_URL = 'https://script.google.com/macros/s/AKfycbw_A0rXmSWOqQb2VTRSx3ewJdZFb94yXEmHqzG5TvYaZcVC1cdUyaNOfKFsE15sIrsy0Q/exec';
-
-// Ejemplo:
-// const API_URL = 'https://script.google.com/macros/s/AKfycbxXXXXXXX.../exec';
