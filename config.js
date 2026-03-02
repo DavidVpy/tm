bc66aff003cb19d6299b38d13d7a9027c7c7b37c
DavidVpy <fdvpfdvp@gmail.com>
2026-03-02 18:01:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ============================================
 // CONFIGURACIÓN - API URL
 // ============================================
-const API_URL = 'https://script.google.com/macros/s/AKfycbxdW5NMCwnAyI-zT_0rIuxPfMe6RBuB_Wg6UxHB3tJmFx2FPnJT4ow4fW0cXyXXSVggrA/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzS6EH865uwQ9doAPWgzM8h0O6qU0xRxINZwO_i-RPmJA2SKzCZQjGicsN8N9bvzfbAsg/exec';
