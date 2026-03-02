c801e30de4f9afd47791f4f4d8c8a31d50188012
DavidVpy <fdvpfdvp@gmail.com>
2026-03-02 19:24:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ============================================
 // CONFIGURACIÓN - API URL
 // ============================================
-const API_URL = 'https://script.google.com/macros/s/AKfycbwHa5RwKMaYoEuTVk9vdXRnljwwmSZTtp1QQuuBVdVcYwuduwHp0Mamke4meWoQSTSPlQ/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwGQyQlFbSOn6bakcMW1ZV3HdiFK6nktkYXrLvROjOXqwQMvH9SC2N27mx8DoNDE6i_dA/exec';
